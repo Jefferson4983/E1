7e8994bb5ad5e5fb5d8c8c9668995876d8ca4d9a
Jefferson4983 <jefferson.caicedo.arturo@utelvt.edu.ec>
2022-09-29 17:31:40 -0500

    Programa de compara.cpp modificado

---
+++ compara.cpp
@@ -0,0 +1,27 @@
+//JeffersonCaicedoArturo
+//29-09-2022 modificado
+#include<iostream>
+using namespace std;
+int main()
+{
+  float x,j;
+  cout<<"Ingrese x  : "; cin>>x;
+  cout<<"Ingrese j  : "; cin>>j;
+  if(x==j){
+     cout<<"Son iguales"<<endl;
+  }else{
+     if(x<j){
+       cout<<x<<" es el menor que "<<j<<"\n";
+     }else{
+       cout<<j<<" es el menor que "<<x<<"\n";
+     }
+  }
+
+return (0);;
+
+
+
+
+
+
+}
